3841c514c6d366a82d48d447963fcd92a4a88023
jordansaker <jordan.saker@ymail.com>
2023-05-04 19:23:32 +1000

    imported file handling and new reference functions into main and created main loop match case. 'new reference' match case flow control structure done. Just need to add conditional statements for different reference types. new reference is printed to references.txt file as desired

---
+++ src/main.py
@@ -0,0 +1,51 @@
+from file_handling import file_handler
+from new_reference import new_reference
+import os.path
+
+
+
+# select which modules to run
+'''
+    The following condition statements will select which 
+    modules to run based on what the user inputs.
+'''
+# ask user whether they want to handle a file or not
+print('\n')
+print('***** Harvard Referencing for Markdown *****')
+
+# Check is references.txt exists. If yes, ask user what they want to do
+if os.path.isfile('references.txt'):
+    print("\nWhat would you like to do? \n\nOptions: 'Insert references', 'Insert citations', 'New Reference'. \nType '\help' to print help document. Type '\quit' to exit.")
+    module_to_run = input('\n>> ').lower()  
+# if not get user to enter new reference
+else:
+    print("\nEnter a reference to get started\n")
+    module_to_run = 'new reference'
+
+# main loop
+match module_to_run:
+# if handle file, ask user for file name
+    case 'insert references':
+        # get the references in references.txt file and place in a list
+
+        # get the markdown file name
+        file_name = input('What is the Markdown file name? \n>> ')
+        file_handler(file_name, '.md', '', 'a+') if '.md' in file_name else file_handler(file_name + '.md', '.md', '', 'a+')
+    case 'insert citations':
+        # get the citations in citations.txt file
+
+        # get the markdown file name
+        pass
+# if not, run the reference module
+    case 'new reference':
+        temporary_reference_list = []
+        end_listing = 0
+        # run new reference function until user types add
+        while end_listing != 'add':
+            end_listing, temporary_reference_list = new_reference(temporary_reference_list)
+        # add the references to the reference.txt file
+        # call the file handler function and write to file
+        file_handler('references.txt', '.txt', ''.join(temporary_reference_list), 'a+')
+    case '\help':
+        # print the help documentation by calling the module function
+        pass
