77cf31947270b95b03cdbb2d622c76ff5fe8a73e
jordansaker <jordan.saker@ymail.com>
2023-05-04 20:21:51 +1000

    added 3 remaining reference types functions

--- src/reference_type.py
+++ src/reference_type.py
@@ -3,15 +3,45 @@ def common_reference_details_inputs():
     title = input('Title: ')
     date_published = input('Date Published (yyyy): ')
     date_accessed = input('Date Accessed (dd/mm/yyyy): ')
-
-    return author_name, title, date_published, date_accessed
-
-def website_reference_details_inputs():
-    website_name = input('Website Name: ')
     URL = input('URL: ')
 
-    return website_name, URL
+    return author_name, title, date_published, date_accessed, URL
 
+
+# Type of Reference Functions ----------------------------------
+def website_reference_details_inputs():
+    website_name = input('Website Name: ')
+
+    return website_name
+
+def book_reference_details_inputs():
+    date_originally_published = input('Date Originally Published: ')
+    edition = input('Edition: ')
+    volume_number = input('Volume Number: ')
+    publisher = input('Publisher: ')
+    publisher_place = input('Publisher Place: ')
+    page_range = input('Page Range: ')
+    ISBN = input('ISBN: ')
+
+    return date_originally_published, edition, volume_number, publisher, publisher_place, page_range, ISBN
+
+def journal_reference_details_inputs():
+    name_of_journal = input('Journal Name: ')
+    volume_number = input('Volume Number: ')
+    issue_number = input('Issue Number: ')
+    page_range = input('Page Range: ')
+    database_name = input('Database Name: ')
+    DOI = input('DOI: ')
+    
+    return name_of_journal, volume_number, issue_number, page_range, database_name, DOI
+
+def video_reference_details_inputs():
+    publisher = input('Publisher: ')
+    video_format = input('Format: ')
+
+    return publisher, video_format
+
+# Reference Builder functions   --------------------------------   
 def website_reference_builder(author_name, title, date_published, date_accessed, website_name, URL):
     
     if author_name:
