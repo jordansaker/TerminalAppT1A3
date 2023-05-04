c2c5b1fd52829af5b71a5f0bf0beaa0ed1015cad
jordansaker <jordan.saker@ymail.com>
2023-05-04 20:02:19 +1000

    added option for user to delete reference list permenantly

--- src/main.py
+++ src/main.py
@@ -13,16 +13,30 @@ import os.path
 print('\n')
 print('***** Harvard Referencing for Markdown *****')
 
+
+# main loop
 # Check is references.txt exists. If yes, ask user what they want to do
 if os.path.isfile('references.txt'):
-    print("\nWhat would you like to do? \n\nOptions: 'Insert references', 'Insert citations', 'New Reference'. \nType '\help' to print help document. Type '\quit' to exit.")
-    module_to_run = input('\n>> ').lower()  
+    print("\nWhat would you like to do? \n\nOptions: 'Insert references', 'Insert citations', 'New Reference'. \nType '\help' to print help document. Type '\quit' to exit. Type '\delete' to delete references list and citations")
+    module_to_run = input('\n>> ').lower()
+
+    # make sure user understands delete
+    if module_to_run in '\delete':
+        print("\nThis will delete all the current references. Are you sure?")
+        # ask user to type Y to confirm N to cancel 
+        user_confirm = input('\nY/N:\n>>')
+        if user_confirm in 'Y':
+            os.remove("references.txt")
+            # os.remove("citations.txt")
+            print("\nReferences deleted successfully")
+            print("\nEnter a reference to get started\n")
+            module_to_run = 'new reference'
 # if not get user to enter new reference
 else:
     print("\nEnter a reference to get started\n")
     module_to_run = 'new reference'
 
-# main loop
+
 match module_to_run:
 # if handle file, ask user for file name
     case 'insert references':
