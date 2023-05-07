8a512199cc9e2a14acbfc398cff54867c35f0487
jordansaker <jordan.saker@ymail.com>
2023-05-07 10:52:25 +1000

    restructured the file handling module. When a markdown document was passed into the original function to be opened and manipulated, the whole document was being deleted. So the new module has separate functions for markdown and text files.

--- src/file_handling.py
+++ src/file_handling.py
@@ -1,25 +1,42 @@
 # File handling module
 '''
-    The module will be a function that receives an argument that is a file name, an argument that contains
-    text the will be added to the file, and a flag to specify whether to read or write.
-    The file will be opened and to be read or written based on the flag.
+    The module contains functions based on specific cases matched in the main loop.
 '''
-def file_handler(filename, file_type, input_text, read_or_write):
+from md_file_search import search_md_for_reference_list_flag
+
+
+
+def insert_references():
+    reference_list = []
     try:
-        with open(filename, read_or_write) as f:
-            # for line in f:
-            #     print(f'Item: {line.strip()}')
-            match file_type:
-                case '.md':
-                    # call md_file_search module
-                    pass
-                case '.txt':
-                    # write input_text to file if TRUE
-                    if input_text:
-                        f.write(input_text)
-                    # Else read from file
-                    else:
-                        pass
+        with open('references.txt', 'r') as file:
+            for line in file:
+                reference_list.append(line)
+
+            # get the markdown file name
+            file_name = input('What is the Markdown file name? \n>> ')
+            # open the markdown file and store in temp file
+            
+            temp_file = search_md_for_reference_list_flag(file_name, reference_list)
+    except OSError as error:
+        print('References List does not exist. Create a list by added a new reference')
+        print(type(error))
+        return type(error)
+        
+
+    # re-write markdown with temp file
+    with open(file_name, 'w') as file:
+        file.writelines(temp_file)
+
+
+def add_new_reference(filename, input_text, read_or_write):
+    try:
+        with open(filename, read_or_write) as file:
+            if input_text:
+                file.write(input_text)
+            # Else display error message
+            else:
+                print('Reference fields are empty. Try again.')
     except OSError as error:
         print('File name not found: %s' % filename)
         print(type(error))
