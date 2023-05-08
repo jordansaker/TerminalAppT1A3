60e79a77c94a49bf1bba150821ff02b6ff8b9f09
jordansaker <jordan.saker@ymail.com>
2023-05-08 22:42:45 +1000

    added insert citations function to module and imported citations search function

--- src/file_handling.py
+++ src/file_handling.py
@@ -2,7 +2,7 @@
 '''
     The module contains functions based on specific cases matched in the main loop.
 '''
-from md_file_search import search_md_for_reference_list_flag
+from md_file_search import search_md_for_reference_list_flag, search_md_for_citation_flags
 
 
 
@@ -28,6 +28,27 @@ def insert_references():
     with open(file_name, 'w') as file:
         file.writelines(temp_file)
 
+def insert_citations():
+    citation_list = []
+    try:
+        with open('citations.txt', 'r') as file:
+            for line in file:
+                citation_list.append(line)
+
+            # get the markdown file name
+            file_name = input('What is the Markdown file name? \n>> ')
+            # open the markdown file and store in temp file
+            
+            temp_file = search_md_for_citation_flags(file_name, citation_list)
+    except OSError as error:
+        print('References List does not exist. Create a list by added a new reference')
+        print(type(error))
+        return type(error)
+        
+
+    # re-write markdown with temp file
+    with open(file_name, 'w') as file:
+        file.writelines(temp_file)
 
 def add_new_reference(filename, input_text, read_or_write):
     try:
