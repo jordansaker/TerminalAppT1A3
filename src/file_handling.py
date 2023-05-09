408629efcb985f6c9496f0eea43b5af43b66073a
jordansaker <jordan.saker@ymail.com>
2023-05-09 13:30:45 +1000

    updated first functiion to take in search or read string arguments. Updated add function to write lines with a for loop

--- src/file_handling.py
+++ src/file_handling.py
@@ -3,10 +3,10 @@
     The module contains functions based on specific cases matched in the main loop.
 '''
 from md_file_search import search_md_for_reference_list_flag, search_md_for_citation_flags
+from txt_file_search import reference_search
 
 
-
-def insert_references_citations(file_type):
+def insert_references_citations(file_type, read_or_search):
     reference_citation_list = []
     try:
         with open(file_type, 'r') as file:
@@ -17,38 +17,43 @@ def insert_references_citations(file_type):
                     reference_citation_list.append(line)
 
             # get the markdown file name
-            file_name = input('What is the Markdown file name? \n>> ')
+            if read_or_search in 'read':
+                file_name = input('What is the Markdown file name? \n>> ')
             # open the markdown file and store in temp file
-            
-            if file_type in 'references.txt':
+            if file_type in 'references.txt' and read_or_search in 'read':
                 temp_file = search_md_for_reference_list_flag(file_name, reference_citation_list)
-            else:
+            elif file_type in 'citations.txt' and read_or_search in 'read':
                 temp_file = search_md_for_citation_flags(file_name, reference_citation_list)
+
     except OSError as error:
         print('References List does not exist. Create a list by added a new reference')
         print(type(error))
         return type(error)
         
-
+    # search for references
+    if file_type in 'references.txt' and read_or_search in 'search':
+        reference_number = reference_search(reference_citation_list)
+        return reference_number, reference_citation_list
+    if file_type in 'citations.txt' and read_or_search in 'search':
+        return reference_citation_list
     # re-write markdown with temp file
-    with open(file_name, 'w') as file:
-        file.writelines(temp_file)
-        
+    if read_or_search in 'read':
+        with open(file_name, 'w') as file:
+            file.writelines(temp_file)
+    else:
+        pass
 
-    # re-write markdown with temp file
-    with open(file_name, 'w') as file:
-        file.writelines(temp_file)
 
 def add_new_reference(filename, input_text, read_or_write):
     try:
         with open(filename, read_or_write) as file:
             if input_text:
-                file.write(input_text)
             # Else display error message
+                for line in input_text:
+                    file.write(line)
             else:
                 print('Reference fields are empty. Try again.')
     except OSError as error:
         print('File name not found: %s' % filename)
         print(type(error))
         return type(error)
-
