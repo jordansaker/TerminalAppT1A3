0cf2724474ab2b1b2dd5ada85477d3c2d51148eb
jordansaker <jordan.saker@ymail.com>
2023-05-10 13:00:40 +1000

    completed the search module with for loop breaks to lower search time

--- src/txt_file_search.py
+++ src/txt_file_search.py
@@ -11,24 +11,35 @@ def reference_search(references_list):
             print(f'{reference}')
         
         print('\nSelect a reference above by author\'s last name or by reference number')
-        query_value = input("Author's Last Name Or Reference Number (Type '\quit' to exit search): ")
+        query_value = input("Author's Last Name Or Reference Number (Type '\quit' to exit search):\n>> ")
 
-        if query_value.isnumeric():
+        if query_value in '\quit':
+            return query_value
+        elif query_value.isnumeric():
             reference_number = int(query_value)
             print(f'{references_list[reference_number]}')
         else:
+            counter = 0
             for reference in references_list:
                 split_reference = reference.split(' ')
                 exp_to_search = query_value.lower() + ','
+                
                 for string in split_reference:
-                    if string.lower() in exp_to_search:
+                    # end for loop once title block is reached
+                    if re.findall("[*]", string):
+                        break
+                    elif string.lower() in exp_to_search:
                         print(f'\n{reference}')
-            else:
-                print("\nreference doesn't exist")
+                        counter += 1
+                        break
+            if not counter:
+                print("\nReference doesn't exist")
         
         print("\nTo edit a reference, select the reference by it's reference number")
         query_value = input("Enter a reference number (Type '\quit' to exit):\n>> ")
 
         if query_value.isnumeric():
             return int(query_value)
+        elif query_value in '\quit':
+            return query_value
 
