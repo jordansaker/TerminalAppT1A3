f2d2ed34001a83be415f7c65cb5ec15ff4c4d0e3
jordansaker <jordan.saker@ymail.com>
2023-05-11 21:11:24 +1000

    added comments to code

--- src/md_file_search.py
+++ src/md_file_search.py
@@ -24,19 +24,25 @@ def search_md_for_citation_flags(md_file, citation_list):
 
         for index, line in enumerate(file):
             temp_file.append(line)
+            # the document is searched for flags relating to each citation
             for citation_index, citation in enumerate(citation_list):
                 
+                # skip blank lines
                 if line in "\n":
                     pass
+                # search for current index flag in line
                 elif f'[{citation_index}]' in line.lower().strip() or \
                         f'[{citation_index}' in line.lower().strip():
 
+                    # put each flag in their own index in a list
                     line_list = line.strip().split(']')
                     line_list = ''.join(line_list)
                     line_list = line_list.strip().split(' ')
-                     
+
+                    # search each element in the list for the flags in the line 
                     for char_index, str in enumerate(line_list):
 
+                        # possible cases a flag might appear after being split, joined, and then split at spaces
                         exp_to_search = re.search(f'\[{citation_index}', str)
                         exp_to_search_multiple_brackets = re.search(f'\[{citation_index}\[[0-9]', str)
                         exp_to_search_space = re.search(f'\[{citation_index} ', str)
@@ -51,10 +57,16 @@ def search_md_for_citation_flags(md_file, citation_list):
                         elif exp_to_search_space:
                             line_list[char_index] = citation.strip() + ' '
                         elif exp_to_search_multiple_brackets:
+                            # case for when there are multiple flags in an element of the list
+                            # the element is split further into another list
+                            # the for loop is then used to add the [ back to each number
+                            # the first indexed flag is inserted with the citations 
+                            # and the string, "(In-text citation) [number ..."
+                            # is used to rewrite the line
                             new_str_list = line_list[char_index].split('[')
                             
                             spaces_between = []
-                            
+
                             for list_item in new_str_list:
                                 if list_item != '':
                                     list_item = '[' + list_item
@@ -69,6 +81,7 @@ def search_md_for_citation_flags(md_file, citation_list):
                         elif exp_to_search:
                             line_list[char_index] = citation.strip()
 
+                    # the list is join and the string is written to the temp_file
                         inserted_citation_line = ' '.join(line_list)
                         line = inserted_citation_line
                     line_index = index
@@ -76,4 +89,3 @@ def search_md_for_citation_flags(md_file, citation_list):
                     temp_file[line_index] = inserted_citation_line + '\n'
 
         return temp_file
-
