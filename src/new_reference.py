eef8032997b16dbcdce168aa193011713f2a65eb
jordansaker <jordan.saker@ymail.com>
2023-05-11 23:09:10 +1000

    changed 'in' to '==' and reduced number of characters in lines

--- src/new_reference.py
+++ src/new_reference.py
@@ -1,13 +1,18 @@
-from reference_type import common_reference_details_inputs, website_reference_details_inputs, website_reference_builder, book_reference_details_inputs, book_reference_builder, journal_reference_details_inputs, journal_reference_builder, video_reference_details_inputs, video_reference_builder
+from reference_type import (common_reference_details_inputs, 
+                                website_reference_details_inputs, website_reference_builder, 
+                                    book_reference_details_inputs, book_reference_builder, 
+                                        journal_reference_details_inputs, journal_reference_builder, 
+                                            video_reference_details_inputs, video_reference_builder)
 
 def new_reference(temporary_reference_list, temporary_citation_list):
     # ask for reference type
     reference_type = input(
         "\nType of refereence: (Website, Book, Journal, Video)\n Or type 'add' to add references to reference list. Type '\quit' to return to main (This won't save any references you've added)\n>> ").lower()
     
-    if reference_type in 'add':
-        return reference_type, temporary_reference_list, temporary_citation_list
-    elif reference_type in '\quit':
+    if reference_type == 'add':
+        return (reference_type, temporary_reference_list,
+                 temporary_citation_list)
+    elif reference_type == '\quit':
         reference_type = 'add'
         return reference_type, [], []
      
@@ -18,10 +23,12 @@ def new_reference(temporary_reference_list, temporary_citation_list):
     # insert citations to temporary citation list
     temporary_citation_list.append(citation)
 
-    return reference_type, temporary_reference_list, temporary_citation_list
+    return (reference_type, temporary_reference_list,
+             temporary_citation_list)
    
 
-def edit_reference(reference_citation_list, reference_number, reference_or_citation, new_citation):
+def edit_reference(reference_citation_list, 
+                   reference_number, reference_or_citation, new_citation):
     # ask for reference type
     if reference_or_citation in 'reference':
         reference_type = input(
@@ -46,18 +53,28 @@ def inputs_function(reference_type):
         case 'website':
             website_name = website_reference_details_inputs()
             # call reference building function for reference type
-            reference, citation = website_reference_builder(author_name, title, date_published, date_accessed, website_name, URL, author_name_citation)
+            reference, citation = website_reference_builder(author_name,
+                                                             title, date_published, date_accessed, 
+                                                                website_name, URL, author_name_citation)
         case 'book':
             chapter_title, edition, volume_number, publisher, publisher_place, page_range = book_reference_details_inputs()
 
-            reference, citation = book_reference_builder(author_name, title, date_published, date_accessed, URL, chapter_title, edition, volume_number, publisher, publisher_place, page_range, author_name_citation)
+            reference, citation = book_reference_builder(author_name, title, date_published,
+                                                          date_accessed, URL, chapter_title, edition,
+                                                            volume_number, publisher, publisher_place, page_range,
+                                                              author_name_citation)
         case 'journal':
             journal_name, volume_number, issue_number, page_range = journal_reference_details_inputs()
 
-            reference, citation = journal_reference_builder(author_name, title, date_published, date_accessed, URL, journal_name, volume_number, issue_number, page_range, author_name_citation)
+            reference, citation = journal_reference_builder(author_name,
+                                                             title, date_published, date_accessed, URL,
+                                                               journal_name, volume_number, issue_number,
+                                                                 page_range, author_name_citation)
         case 'video':
             publisher, video_format = video_reference_details_inputs()
 
-            reference, citation = video_reference_builder(author_name, title, date_published, date_accessed, URL, publisher, video_format, author_name_citation)
+            reference, citation = video_reference_builder(author_name, title,
+                                                           date_published, date_accessed, URL,
+                                                             publisher, video_format, author_name_citation)
 
     return reference, citation
