17cb9a1a054354bd85410f075de0153daddc0f59
jordansaker <jordan.saker@ymail.com>
2023-05-11 23:04:04 +1000

    reduced return block to a single line

--- src/txt_file_search.py
+++ src/txt_file_search.py
@@ -11,35 +11,40 @@ def reference_search(references_list):
             print(f'{reference}')
         
         print('\nSelect a reference above by author\'s last name or by reference number')
-        query_value = input("Author's Last Name Or Reference Number (Type '\quit' to exit search):\n>> ")
 
-        if query_value in '\quit':
-            return query_value
-        elif query_value.isnumeric():
-            reference_number = int(query_value)
-            print(f'{references_list[reference_number]}')
-        else:
-            counter = 0
-            for reference in references_list:
-                split_reference = reference.split(' ')
-                exp_to_search = query_value.lower() + ','
-                
-                for string in split_reference:
-                    # end for loop once title block is reached
-                    if re.findall("[*]", string):
-                        break
-                    elif string.lower() in exp_to_search:
-                        print(f'\n{reference}')
-                        counter += 1
-                        break
-            if not counter:
-                print("\nReference doesn't exist")
+        err = False
+        while not err:
+            try:
+                query_value = input("\nAuthor's Last Name Or Reference Number (Type '\quit' to exit search):\n>> ")
+
+                if query_value == '\quit':
+                    return query_value
+                elif query_value.isnumeric():
+                    reference_number = int(query_value)
+                    print(f'\n{references_list[reference_number - 1]}')
+                    break
+                else:
+                    counter = 0
+                    for reference in references_list:
+                        split_reference = reference.split(' ')
+                        exp_to_search = query_value.lower() + ','
+                        
+                        for string in split_reference:
+                            # end for loop once title block is reached
+                            if re.findall("[*]", string):
+                                break
+                            elif string.lower() in exp_to_search:
+                                print(f'\n{reference}')
+                                counter += 1
+                                break
+                    if not counter:
+                        raise IndexError
+
+            except IndexError:
+                print("\nNo reference exists")
         
         print("\nTo edit a reference, select the reference by it's reference number")
-        query_value = input("Enter a reference number (Type '\quit' to exit):\n>> ")
+        query_value = input("\nEnter a reference number (Type '\quit' to exit):\n>> ")
 
-        if query_value.isnumeric():
-            return int(query_value)
-        elif query_value in '\quit':
-            return query_value
+        return query_value
 
