b8880ec06ac375b6161fb6163c149b9673e0438e
jordansaker <jordan.saker@ymail.com>
2023-05-12 09:44:45 +1000

    added colour to text

--- src/txt_file_search.py
+++ src/txt_file_search.py
@@ -1,27 +1,28 @@
 import re
+from colorist import Color
 
 def reference_search(references_list):
     query_value = ''
     while query_value != '\quit':
-        print('\n***** SEARCH *****')
-        print('''\n This option allows you to select a reference and edit the entire reference ''')
+        print(f'\n{Color.CYAN}***** SEARCH *****{Color.OFF}')
+        print('''\n This option allows you to select a reference and edit or delete the entire reference ''')
         print('\nReferences: \n')
         
         for reference in references_list:
-            print(f'{reference}')
+            print(f'{Color.WHITE}{reference}{Color.OFF}')
         
         print('\nSelect a reference above by author\'s last name or by reference number')
 
         err = False
         while not err:
             try:
-                query_value = input("\nAuthor's Last Name Or Reference Number (Type '\quit' to exit search):\n>> ")
+                query_value = input(f"\nAuthor's Last Name Or Reference Number (Type {Color.MAGENTA} '\quit' {Color.OFF} to exit search):\n>> ")
 
                 if query_value == '\quit':
                     return query_value
                 elif query_value.isnumeric():
                     reference_number = int(query_value)
-                    print(f'\n{references_list[reference_number - 1]}')
+                    print(f'\n{Color.WHITE}{references_list[reference_number - 1]}{Color.OFF}')
                     break
                 else:
                     counter = 0
@@ -41,10 +42,10 @@ def reference_search(references_list):
                         raise IndexError
 
             except IndexError:
-                print("\nNo reference exists")
+                print(f"\n{Color.RED}No reference exists{Color.OFF}")
         
         print("\nTo edit a reference, select the reference by it's reference number")
-        query_value = input("\nEnter a reference number (Type '\quit' to exit):\n>> ")
+        query_value = input(f"\nEnter a reference number (Type {Color.MAGENTA} '\quit' {Color.OFF} to exit):\n>> ")
 
         return query_value
 
