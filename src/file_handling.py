805c1ade2f69c5e4192f7aa752cdb96ed3e79b2d
jordansaker <jordan.saker@ymail.com>
2023-05-11 23:01:35 +1000

    added a regex to strip reference numbers each time the file is opened and re-assign numbers to account for list updates

--- src/file_handling.py
+++ src/file_handling.py
@@ -4,6 +4,7 @@
 '''
 from md_file_search import search_md_for_reference_list_flag, search_md_for_citation_flags
 from txt_file_search import reference_search
+import re
 
 
 def insert_references_citations(file_type, read_or_search):
@@ -12,27 +13,38 @@ def insert_references_citations(file_type, read_or_search):
         with open(file_type, 'r') as file:
             for index, line in enumerate(file):
                 if file_type in 'references.txt':
+                    
+                    exp_to_search = re.search('[0-9]', line)
+
+                    if exp_to_search:
+                        list_line = line.split(' ')
+                        del list_line[0]
+                        line = ' '.join(list_line)
+                    
                     numbered_reference = f'{index + 1}. {line}'
                     reference_citation_list.append(numbered_reference)
                 else:
                     reference_citation_list.append(line)
-
+            add_new_reference('references.txt', reference_citation_list, 'w')
             # get the markdown file name
             if read_or_search in 'read':
-                file_name = input("What is the Markdown file name? (Type '\quit' to exit to main menu) \n>> ")
+                file_name = input(
+                    "What is the Markdown file name? (Type '\quit' to exit to main menu) \n>> ")
                 if file_name in '\quit':
                     return None
             # open the markdown file and store in temp file
             if file_type in 'references.txt' and read_or_search in 'read':
-                temp_file = search_md_for_reference_list_flag(file_name, reference_citation_list)
+                temp_file = search_md_for_reference_list_flag(
+                    file_name, reference_citation_list)
             elif file_type in 'citations.txt' and read_or_search in 'read':
-                temp_file = search_md_for_citation_flags(file_name, reference_citation_list)
+                temp_file = search_md_for_citation_flags(
+                    file_name, reference_citation_list)
 
     except OSError as error:
         print('References List does not exist. Create a list by added a new reference')
         print(type(error))
         return type(error)
-        
+
     # search for references
     if file_type in 'references.txt' and read_or_search in 'search':
         reference_number = reference_search(reference_citation_list)
@@ -46,12 +58,11 @@ def insert_references_citations(file_type, read_or_search):
     else:
         pass
 
-
 def add_new_reference(filename, input_text, read_or_write):
     try:
         with open(filename, read_or_write) as file:
             if input_text:
-            # Else display error message
+                # Else display error message
                 for line in input_text:
                     file.write(line)
             else:
